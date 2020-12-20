521986b0a64b4c477c414401cdc7d2afd3b1abb7
dtinth on Cloud9 <dtinth@spacet.me>
2020-12-20 11:24:47 +0000

    Extract dtinth-water to its own file

--- layouts/default.vue
+++ layouts/default.vue
@@ -16,54 +16,7 @@ export default {
 
 <style>
 @import '../vendor/raster.grid.css';
-
-:root[data-dtinth] {
-  --background-body: #353433;
-  --background: #090807;
-  --background-alt: #252423;
-  --selection: #8b8685;
-  --text-main: #e9e8e7;
-  --text-bright: #fff;
-  --text-muted: #8b8685;
-  --links: #ffb;
-  --code: #e9d7c5;
-  --scrollbar-thumb: #d7fc70;
-}
-[data-dtinth] body {
-  font-family: Arimo, Helvetica, -apple-system, BlinkMacSystemFont, Segoe UI,
-    Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-    sans-serif;
-}
-[data-dtinth] body {
-  line-height: 1.64;
-}
-[data-dtinth] H2 {
-  color: #d7fc70;
-  text-shadow: 2px 2px RGBA(215, 252, 112, 0.15);
-}
-[data-dtinth] H3 {
-  color: #febfea;
-  text-shadow: 2px 2px RGBA(0, 0, 0, 0.25);
-}
-[data-dtinth] CODE {
-  background: #252423;
-  font-family: Comic Mono, Menlo, Consolas, sans-serif;
-}
-[data-dtinth] :not(PRE) > CODE {
-  border: 1px solid #555453;
-  box-shadow: 0 1px 0 #252423;
-  border-radius: 2px;
-  border-bottom-width: 2px;
-  background: #252423;
-}
-[data-dtinth] FIGURE {
-  text-align: center;
-}
-[data-dtinth] FIGCAPTION {
-  font-style: italic;
-  color: #bef;
-  margin-top: -1em;
-}
+@import '../assets/dtinth-water.css';
 
 a.header-anchor {
   font-size: 0.85em;
