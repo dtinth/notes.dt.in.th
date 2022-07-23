c3c3e7ff9be7f3dbfb19f06d2c36b225529b3c18
dtinth on MBP M1 <dtinth@spacet.me>
2022-07-23 14:34:52 +0700

    Import stylings

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,29 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@fontsource/arimo/400.css";
+import "@fontsource/arimo/400-italic.css";
+import "@fontsource/arimo/500.css";
+import "@fontsource/arimo/500-italic.css";
+import "@fontsource/arimo/600.css";
+import "@fontsource/arimo/600-italic.css";
+import "@fontsource/arimo/700.css";
+import "@fontsource/arimo/700-italic.css";
+import "comic-mono/index.css";
+import "water.css/out/dark.css";
+import "../vendor/raster.grid.css";
+import "../assets/dtinth-water.css";
+
+import type { AppProps } from "next/app";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>My page title</title>
+        <meta property="og:title" content="My page title" key="title" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ pages/_document.tsx
@@ -0,0 +1,13 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html data-dtinth>
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
