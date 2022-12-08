450c6d5b85858d835d71a3755b224a39de87c2d0
dtinth on MBP M1 <dtinth@spacet.me>
2022-12-09 00:27:35 +0700

    Go left!

--- src/packlets/ui/Header.tsx
+++ src/packlets/ui/Header.tsx
@@ -8,11 +8,9 @@ export const Header: FC<Header> = (props) => {
   return (
     <>
       <header className={classes.header}>
-        <div className={classes.container}>
-          <div className={classes.section}>
-            <div className={classes.title}>
-              <Link href="/">notes.dt.in.th</Link>
-            </div>
+        <div className={classes.section}>
+          <div className={classes.title}>
+            <Link href="/">notes.dt.in.th</Link>
           </div>
         </div>
       </header>
